be22527a96483448b7457cc71bb52313aa02e1de
rankell270 <henrique.rankel8@gmail.com>
2025-04-03 08:17:41 -0300

    Create banco-festas.sql

---
+++ banco-festas.sql
@@ -0,0 +1,30 @@
+CREATE TABLE clientes 
+( 
+); 
+
+CREATE TABLE festas 
+( 
+); 
+
+CREATE TABLE pagamento 
+( 
+); 
+
+CREATE TABLE boleto 
+( 
+ data_vencimento INT,  
+ cod_barras INT,  
+); 
+
+CREATE TABLE pix 
+( 
+ cod-pix INT,  
+); 
+
+CREATE TABLE cartaocredito 
+( 
+ endereco INT,  
+ nome no cartão INT,  
+ cpf INT,  
+); 
+
